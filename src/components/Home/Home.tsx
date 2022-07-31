3cf6818e9b2314ceae45de91c3bd9d1aa1a5aa43
CathalButler <cathalbutler@outlook.com>
2022-07-31 23:17:35 +0800

    Added Material UI. Initial home layout created

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,48 @@
+import "./Home.css"
 
 const Home = () => {
 
-    return(
-        <h2>
-            Hello Page
-        </h2>
+
+    return (
+
+        <div className={"page-wrapper"}>
+            <div className={"main-wrapper"}>
+                <div className={"nav-bar"}>
+                    <div className={"nav-bar-container"}></div>
+                </div>
+
+                <div className={"section-todo-hero"}>
+                    <div className={"page-padding"}>
+                        <div className={"container-large"}>
+                            <div className={"padding-vertical padding-small"}>
+                                <h2>Welcome to today todo List</h2>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"section-todo-body"}>
+                    <div className={"page-padding"}>
+                        <div className={"container-large"}>
+                            <div className={"padding-vertical padding-small"}>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className={"section-todo-footer"}>
+
+
+                </div>
+
+            </div>
+        </div>
+
+
     )
 }
 
