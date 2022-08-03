18f20c00609be58a755a2b80b70ac0d010e6b390
CathalButler <cathalbutler@outlook.com>
2022-08-03 14:13:59 +0800

    Minor refactoring

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,13 +14,16 @@ const Home = () => {
                             <a href={"#"} className={"brand"}></a>
                             <nav className={"navbar-nav-container"}>
                                 <a href={"#"} className={"home-nav"}>
-                                    <img src="https://img.icons8.com/material-outlined/24/000000/home--v2.png"/><img/>
+                                    <img src="https://img.icons8.com/material-outlined/24/000000/home--v2.png"
+                                         alt={"Home Icon"}/>
                                 </a>
                                 <a href={"#"} className={"fav-nav"}>
-                                    <img src="https://img.icons8.com/material-outlined/24/000000/hearts.png"/>
+                                    <img src="https://img.icons8.com/material-outlined/24/000000/hearts.png"
+                                         alt={"Favourites Icon"}/>
                                 </a>
                                 <a href={"#"} className={"more-nav"}>
-                                    <img src="https://img.icons8.com/material-outlined/24/000000/more.png"/>
+                                    <img src="https://img.icons8.com/material-outlined/24/000000/more.png"
+                                         alt={"More Icon"}/>
                                 </a>
                             </nav>
                         </div>
@@ -38,7 +41,8 @@ const Home = () => {
                                                 <div className={"tag-column-item-1 w-col w-col-2"}>
                                                     <img
                                                         src="https://img.icons8.com/material-outlined/24/000000/circled.png"
-                                                        height={"24"} width={"24"} className={"image"}/>
+                                                        height={"24"} width={"24"} className={"image"}
+                                                        alt={"Circle Icon"}/>
                                                 </div>
                                                 {/*TODO - Change from hardcoded value to what will be read from the DB*/}
                                                 <div className={"tag-column-item-2 w-col w-col-10"}>
@@ -116,21 +120,23 @@ const Home = () => {
                                             <label className={"w-checkbox todo-checkbox-field"}>
                                                 <div className={"w-checkbox todo-checkbox"}></div>
                                                 <input name={"checkbox"} type={"checkbox"}></input>
-                                                <span className={"todo-checkbox-label w-form-label"}> Create Login UI</span>
+                                                <span
+                                                    className={"todo-checkbox-label w-form-label"}> Create Login UI</span>
                                             </label>
 
                                             <label className={"w-checkbox todo-checkbox-field"}>
                                                 <div className={"w-checkbox todo-checkbox"}></div>
                                                 <input name={"checkbox"} type={"checkbox"}></input>
-                                                <span className={"todo-checkbox-label w-form-label"}> Create SignUp UI</span>
+                                                <span
+                                                    className={"todo-checkbox-label w-form-label"}> Create SignUp UI</span>
                                             </label>
 
                                             <label className={"w-checkbox todo-checkbox-field"}>
                                                 <div className={"w-checkbox todo-checkbox"}></div>
                                                 <input name={"checkbox"} type={"checkbox"}></input>
-                                                <span className={"todo-checkbox-label w-form-label"}> Create Home UI</span>
+                                                <span
+                                                    className={"todo-checkbox-label w-form-label"}> Create Home UI</span>
                                             </label>
-
 
 
                                         </form>
