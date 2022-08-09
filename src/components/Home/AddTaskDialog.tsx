a718d665bb20ef16748c925d5a3e63030957b08f
CathalButler <cathalbutler@outlook.com>
2022-08-09 17:48:53 +0800

    Form state implemented for adding a new task

--- src/components/Home/AddTaskDialog.tsx
+++ src/components/Home/AddTaskDialog.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -8,6 +9,14 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import {TransitionProps} from '@mui/material/transitions';
 import {TextField} from "@mui/material";
+import {gql, useMutation} from "@apollo/client";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {ErrorMessage} from "../../errorMessage";
+
+interface TaskDetails {
+    title: string,
+    category: string
+}
 
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & {
@@ -18,10 +27,45 @@ const Transition = React.forwardRef(function Transition(
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
-// Todo - write code to push form data t
+// Todo - write code to push form data
+const CREATE_NEW_TASK_MUTATION = gql`
+    mutation CreateMutation(
+        $title: String!
+        $category: String!
+    ) {
+        createTask(title: $title, category: $category) {
+            id
+            title
+            category
+            createdAt
+        }
+    }
+`;
 
 export default function AddTaskDialog() {
+
+    //Constants:
     const [open, setOpen] = React.useState(false);
+    const {register, handleSubmit, formState: {errors}} = useForm<TaskDetails>();
+    // TODO - Add error handling
+    const [error, setError] = useState<ErrorMessage>()
+
+    const [addTask] = useMutation(CREATE_NEW_TASK_MUTATION,
+        {
+            onCompleted: ({addTask}) => {
+                console.log(addTask);
+                //TODO - Close Dialog box
+            },
+        }
+    );
+
+    const onSubmit: SubmitHandler<TaskDetails> = data => addTask(
+        {
+            variables: {title: data.title, category: data.category}
+        },
+    ).then(({data}) => {
+        console.log(data)
+    })
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -36,41 +80,53 @@ export default function AddTaskDialog() {
             <Button variant="contained" color={"secondary"} onClick={handleClickOpen}>
                 Add Task
             </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Add a new task</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Name
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Task Name"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                    />
 
-                    <div className={"padding padding-small"}/>
-                    <DialogContentText>
-                        Category
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Category"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button color={"error"} onClick={handleClose}>Cancel</Button>
-                    <Button variant="contained" color={"secondary"} onClick={handleClose}>Submit</Button>
-                </DialogActions>
-            </Dialog>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Dialog open={open} onClose={handleClose} TransitionComponent={Transition} disablePortal>
+                    <DialogTitle>Add a new task</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Name
+                        </DialogContentText>
+                        <TextField
+                            {...register("title", {
+                                required: true,
+                                min: 4
+                            })}
+                            autoFocus
+                            margin="dense"
+                            id="title"
+                            label="Task Name"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                        />
+                        <div className={"padding padding-small"}/>
+
+
+                        <DialogContentText>
+                            Category
+                        </DialogContentText>
+                        <TextField
+                            {...register("category", {
+                                required: true,
+                                min: 4
+                            })}
+                            autoFocus
+                            margin="dense"
+                            id="category"
+                            label="Category"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button color={"error"} onClick={handleClose}>Cancel</Button>
+                        <Button variant="contained" color={"secondary"} type={"submit"}>Submit</Button>
+                    </DialogActions>
+                </Dialog>
+            </form>
         </div>
     );
 }
