5359ffb91fe1f04db1602366b25f890b9745fae1
CathalButler <cathalbutler@outlook.com>
2022-08-24 16:55:10 +0800

    Fixed an issue with navigator

--- src/components/Home/MainNavbar.tsx
+++ src/components/Home/MainNavbar.tsx
@@ -4,12 +4,8 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
 import {AUTH_TOKEN} from "../../constants";
-import {useNavigate} from "react-router-dom";
 
 const MainNavbar = () => {
-    //Constants
-    const navigate = useNavigate();
-
     return (
         <div className={"navbar-body"}>
             <div className={"MainNavbar"}>
@@ -24,8 +20,7 @@ const MainNavbar = () => {
                         </IconButton>
                         <IconButton color={"primary"} onClick={() => {
                             localStorage.removeItem(AUTH_TOKEN);
-                            navigate('/login')
-                        }}>
+                        }} href={"/login"}>
                             <LogoutIcon/>
                         </IconButton>
                     </nav>
